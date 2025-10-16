aa67e5365ed952d8e5dcaa9f7220128760703994
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-10-16 11:58:36 -0400

    Gets distance

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sultan 
+ * Created on: Oct 2025
+ * This program gets distance to a object and shows it using a sonar.
 */
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
-basic.showString('Hello, World!')
+// varibles
+let distanceToObject: number = 0
+
+// find disctance from sonar 
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject= sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
